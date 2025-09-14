11e8ceb4b49c0af33b730347393ee215af20cfc9
Darryn <darryndotdev@gmail.com>
2025-09-14 12:30:21 +0100

    feat: adding dark mode toggle

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
 import styles from './Hero.module.css';
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
 
 const Hero = () => {
     return (
         <header className={styles.hero}>
-            <h1>Convert text to HTML</h1>
+            <div>
+                <h1>Convert text to HTML</h1>
+                <DarkModeToggle />
+            </div>
             <p>
                 A powerful tool designed to save you time and reduce errors by
                 transforming plain text into clean and structured HTML.
